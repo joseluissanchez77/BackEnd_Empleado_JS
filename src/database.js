5364653a8f463e1e590e3699854c982706a5925b
joseluissanchez77 <joseluissanchez41@hotmail.com>
2023-09-30 11:56:24 -0500

    crede

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const URI = 'mongodb://127.0.0.1/usuarios_db';
-const URI = 'mongodb+srv://areaspepelucho:0931147284@joseups.orftvmm.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp';
+const URI = 'mongodb://127.0.0.1/usuarios_db';
+// const URI = 'mongodb+srv://areaspepelucho:12345678@joseups.orftvmm.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp';
 
 mongoose.connect(URI)
 .then(db=>console.log('DB CONECTADA'))
